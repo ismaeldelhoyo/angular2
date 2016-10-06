deaa04ee5a37ad875ad8711b8f3dc512511d93c1
Alberto Basalo <albertobasalo@agorabinaria.com>
2016-10-06 11:48:38 +0200

    binding

--- 3-databinding/cash-flow/src/app/movimientos/nuevo/nuevo.component.ts
+++ 3-databinding/cash-flow/src/app/movimientos/nuevo/nuevo.component.ts
@@ -6,21 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nuevo.component.css']
 })
 export class NuevoComponent implements OnInit {
-  tipos: string[] = ["Ingreso", "Gasto"];
-  categorias: string[] = ["Nómina", "Venta", "Intereses"];
-  movimiento = {
-    fecha: new Date(Date.now()),
-    importe: 0,
-    tipo: this.tipos[0]
-  }
+  tipos: any[] = [
+    { id: 1, text: "Ingreso" },
+    { id: 2, text: "Gasto" }];
+  categoriasTipos: any[] = [
+    { id: 1, text: "Nómina", type: 1 },
+    { id: 2, text: "Ventas", type: 1 },
+    { id: 3, text: "Intereses", type: 1 },
+    { id: 4, text: "Hipoteca", type: 2 },
+    { id: 5, text: "Compras", type: 2 },
+    { id: 6, text: "Domicialiaciones", type: 2 },
+    { id: 7, text: "Impuestos", type: 2 }];
+  categorias: any[] = [];
 
+  movimiento: any = {};
 
   constructor() { }
 
   ngOnInit() {
+    this.movimiento = {
+      fecha: new Date(Date.now()),
+      importe: 0,
+      tipo: this.tipos[0].id
+    }
+    this.cambioTipo();
   }
 
-  guardarMovimiento = function () {
+   cambioTipo() {
+    this.categorias = this.categoriasTipos.filter(c => c.type === this.movimiento.tipo);
+  }
+
+  guardarMovimiento() {
     console.log(this.movimiento);
   }
 }
