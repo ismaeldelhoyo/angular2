ee69e811ea9be3919ab7db6ad62df0ab27921035
Alberto Basalo <albertobasalo@hotmail.com>
2016-11-28 15:38:36 +0100

    3

--- 2-modulos/cash-flow/src/app/movimientos/movimientos.module.ts
+++ 2-modulos/cash-flow/src/app/movimientos/movimientos.module.ts
@@ -1,21 +1,25 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-/** Importa un componente que declara y que luego exporta */
+/** Importae un componentes de este módulo */
 import { MovimientosComponent } from './movimientos.component';
 import { ListaComponent } from './lista/lista.component';
 import { NuevoComponent } from './nuevo/nuevo.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],// dependencias de otros módulos
   declarations: [
     MovimientosComponent,
     ListaComponent,
     NuevoComponent
-  ],// Componente que el mismo declara
+  ],// Componentes que se declara en el módulo de movimientos
   exports: [
     MovimientosComponent
-  ] // exporta los componentes importables desde otros módulos
+  ] // exporta el componente que quieras importarlo desde otros módulos
 })
 export class MovimientosModule { }
+
+/** To Do: comprobar exportaciones e importaciones */
--- 3-databinding/readme.md
+++
@@ -1,9 +0,0 @@
-[Data Binding](http://academia-binaria.com/databinding-el-flujo-de-datos-de-angular2/)
-
-Enlace entre plantillas y modelos
-
-### Guía
-- `nuevo.component.html` : directivas para enlace de datos. stributos, propiedades eventos
-- `nuevo.component.ts` : modelo en clase controladora
-
-(https://scotch.io/tutorials/how-to-deal-with-different-form-controls-in-angular-2)
