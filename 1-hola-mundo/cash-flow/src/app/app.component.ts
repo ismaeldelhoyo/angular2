a332b44fcb265c27e120c6860d8a7cd4fc9d8a79
ismaeldelhoyo <ismaeldelhoyo@gmail.com>
2016-11-29 09:22:32 +0100

    Hola mundo

--- 1-hola-mundo/cash-flow/src/app/app.component.ts
+++ 1-hola-mundo/cash-flow/src/app/app.component.ts
@@ -1,14 +1,11 @@
-// importar antes de usar
 import { Component } from '@angular/core';
-// decorador para definir un componente
+
 @Component({
-  selector: 'app-root',// selector para ser consumido como elemento html
-  templateUrl: './app.component.html', // ruta a la plantilla
-  styleUrls: ['./app.component.css'] // estilos en css o sass
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-// los componentes son clases instanciables
 export class AppComponent {
-  // propiedades para enlazar con la plantilla
   title = 'app works!';
   public empresa:string='Hola CSA';
 }
